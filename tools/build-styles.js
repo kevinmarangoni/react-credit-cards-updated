459941d841848c4096cc98d6e2e4894896345954
kevinmarangoni <kevin-marangoni_@hotmail.com>
2022-09-05 18:30:03 -0300

    changes .css building commands

--- tools/build-styles.js
+++ tools/build-styles.js
@@ -4,13 +4,14 @@ const { exec } = require('child_process');
 
 const run = promisify(exec);
 
-run('rm -rf .tmp/')
-  .then(() => run('node-sass -o .tmp/ src/styles.scss'))
+run('rm -rf .tmp; mkdir .tmp')
+  .then(() => run('sass src/styles.scss .tmp/styles.css'))
   .then(() => run('postcss .tmp/styles.css --use autoprefixer --no-map -d .tmp/'))
   .then(() => run('mv .tmp/styles.css .tmp/styles-compiled.css'))
   .then(() => run('mkdir -p es lib'))
   .then(() => run('cp .tmp/styles-compiled.css es/ && cp .tmp/styles-compiled.css lib/'))
-  .then(() => run('cp src/styles.scss es/ && cp src/styles.scss lib/'))
+  .then(() => run('cp src/styles.scss es/ && cp src/styles.scss lib/; rm -rf .tmp'))
+
   .then(() => console.log('✔ Styles have been build'))
   .catch(err => {
     console.error(err);
